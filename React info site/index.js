dc5eec8621e4f753d002c8b26290184af7da52c2
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-07-31 01:31:53 +0300

    JSX
    JSX explaination and render nav element

--- React info site/index.js
+++ React info site/index.js
@@ -1,18 +1,33 @@
-/*Why React?
-    1- it's Composable
-        composability => it goes quite a bit deeper
-        we will make our components mush more composable and reusable
-    2- it's Declarative means what should be done?
-        like I tell him what to do and don't worry about the details OF HOW TO GET IT DONE
-        declarative => it makes our code more readable
-        we will write less code and focus on what the UI should look like
-        imperative => how should it be done 
-        describe to me every step on how to do something, and i will do it
+/*JSk stands for => JavaScript XML
+    a flavor of html into javascript
+ react builds with jsx just old javascript objects
+    and those objects descripe the dom elemetn that we
+    want react to put onto the page for us
+ in JSX i can't render more than two parent elements (like h1 and p)
+    together i have to wrap them in like a div
+
 */
 
-const h1Element = document.createElement('h1');
-h1Element.textContent = 'Hello, React!';
-h1Element.className = 'header';
-//const rootDiv = document.getElementById('root');
-//rootDiv.appendChild(h1Element);
-document.getElementById('root').append(h1Element);
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+/*{type: "h1", key: null, ref: null, props: {className: "header",
+ children: "This is JSX"}, _owner: null, _store: {}}
+ it's a javascript object
+*/
+ReactDOM.render(element, document.getElementById("root"));
+
+
+const nav = (
+    <nav>
+        <h1>website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>  
+);
+ReactDOM.render( 
+    nav,
+    document.getElementById("root")
+);
