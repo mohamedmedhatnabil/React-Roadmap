fe047eda01a053ced30d173adf48428c7df42543
Mohamed Medhat <131720857+mohamedmedhatnabil@users.noreply.github.com>
2024-07-31 00:05:55 +0300

    basic react
    how to render html  h1 and unordered list

---
+++ React info site/index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById("root"));
+ReactDOM.render(
+    <ul><li>One</li><li>Two</li></ul>,
+    document.getElementById("rrot")
+    );
+/*Why React?
+    1- it's composable                                          
+
+*/
